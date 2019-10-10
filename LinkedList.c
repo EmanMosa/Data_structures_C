40b1cddb32f25583ef80c954faa3acd440a409dc
Eman <eman1371994@gmail.com>
2019-10-10 09:52:18 +0300

    delete the main delete the commit change the name of function

--- LinkedList.c
+++ LinkedList.c
@@ -8,39 +8,37 @@ struct node
 };
 struct node* head;
 struct node* tail;
-struct node* insertToTail(int num);
-struct node* insertNodeToIndex(int node, int index) {
-	struct node* in = (struct node*)malloc(sizeof(struct node));
+struct node* Append(int num);
+struct node*  InsertAtHead(int);
+struct node* InsertAtPosition(int num, int index);
+void Print();
+struct node* DeleteAtPosition(int index);
+
+struct node* InsertAtPosition(int num, int index) {
+	int count = 0;
+	struct node* pointer = head;
 	struct node* tmp = (struct node*)malloc(sizeof(struct node));
-	struct node* after = (struct node*)malloc(sizeof(struct node));
-
-	int i = index;
-	tmp = head;
-	in->data = node;
-
-	while (tmp != NULL && i > 0)
-	{
-		after = tmp;
-		if (i == 2 && tmp->next != NULL) {
-
-			in->next = after->next;
-			tmp->next = in;
-			head = tmp;
-			break;
-
-		}
-		else if (tmp->next == NULL && i == 2) {
-			return insertToTail(node);
-		}
-		tmp = tmp->next;
-
-		i--;
-
+	tmp->data = num;
+	tmp->next = NULL;
+	if (index == 0) {
+		head = insertToHead(num);
 
 	}
+	else
+	{
+		while (count!=(index-1))
+		{
+			pointer = pointer->next;
+			count++;
+
+		}
+		tmp->next = pointer->next;
+		pointer->next = tmp;
+	}
+	head = pointer;
 	return head;
 }
-struct node* insertToTail(int num) {
+struct node* Append(int num) {
 	struct node* tmp = (struct node*)malloc(sizeof(struct node));
 	tmp->data = num;
 	tmp->next = NULL;
@@ -57,29 +55,29 @@ struct node* insertToTail(int num) {
 
 
 }
-struct node* insertToHead(int num) {
-	struct node* tmp = (struct node*)malloc(sizeof(struct node));
+struct node* InsertAtHead( int num) {
+	struct node* tmp=(struct node*)malloc(sizeof(struct node));
 
 	tmp->data = num;
 	tmp->next = head;
-	head = tmp;
+	head= tmp;
 
 	return head;
 
 
 }
-void printList() {
+void Print() {
 	struct node* tmp = (struct node*)malloc(sizeof(struct node));
 	tmp = head;
-	while (tmp != NULL)
+	while (tmp!=NULL)
 	{
-
+		
 		printf("%d    ", tmp->data);
 		tmp = tmp->next;
 	}
 
 }
-struct node* DeleteIndex(int index) {
+struct node* DeleteAtPosition(int index) {
 
 	struct node* tmp = (struct node*)malloc(sizeof(struct node));
 	struct node* after = (struct node*)malloc(sizeof(struct node));
@@ -87,19 +85,19 @@ struct node* DeleteIndex(int index) {
 
 	int i = index;
 	tmp = head;
-
+	
 
 	while (tmp != NULL && i > 0)
-	{
+	{	
 		after = tmp->next;
 		if (i == 2 && tmp->next != NULL) {
 
-			tmp->next = after->next;
+			tmp->next =after->next;
 			head = tmp;
 			break;
 
 		}
-
+		
 		tmp = tmp->next;
 
 		i--;
@@ -108,30 +106,4 @@ struct node* DeleteIndex(int index) {
 	}
 	return head;
 }
-void main() {
 
-	struct node* head = (struct node*)malloc(sizeof(struct node));
-	struct node* tail = (struct node*)malloc(sizeof(struct node));
-	head = tail;
-	int num;
-	int node;
-	printf("how mony node do you want :");
-	scanf_s("%d", &num);
-	for (int i = 1; i <= num; i++) {
-		printf("enter the node number %d :     ", i);
-		scanf_s("%d", &node);
-		//head=insertToHead(node);
-		head = insertToTail(node);
-	}
-	head = insertNodeToIndex(5, 2);
-	head = insertNodeToIndex(5, 3);
-	/*head = insertNodeToIndex(5, 6);
-	head = insertNodeToIndex(5,8);
-	head = insertNodeToIndex(5, 10);*/
-	head = DeleteIndex(2);
-	head = DeleteIndex(5);
-
-	printList();
-
-
-}
